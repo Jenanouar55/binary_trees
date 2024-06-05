3b86983072f5696ff203421a6b1f9d8b89db13df
jenanouar1234 <jenanouar1234@gmail.com>
2024-06-05 20:18:30 +0100

    pushed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,11 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
+
 	if (tree == NULL)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
@@ -25,9 +27,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left_height = 0, right_height = 0;
+
 	if (tree == NULL)
 		return (0);
-	left_height = (tree->left) ? (int)binary_tree_height(tree->left) + 1 : 0;
-	right_height = (tree->right) ? (int)binary_tree_height(tree->right) + 1 : 0;
+
+	if (tree->left)
+		left_height = (int)binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_height = (int)binary_tree_height(tree->right) + 1;
 	return (left_height - right_height);
 }
